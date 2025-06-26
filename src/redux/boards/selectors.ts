0a1644f22ac2b40dd216e6c09a5d2b4bcefd83d9
lesia_onyshchuk <lkane443@gmail.com>
2025-06-26 17:00:09 +0300

    fix ts

--- src/redux/boards/selectors.ts
+++ src/redux/boards/selectors.ts
@@ -4,8 +4,7 @@ import { Board } from "./slice";
 export const selectCurrentBoard = (state: RootState): Board | null =>
   state.boards.board;
 
-export const selectBoardLoading = (state: RootState): boolean =>
-  state.boards.loading;
+export const selectBoardLoading = (state: RootState) => state.boards.loading;
 
 export const selectError = (state: RootState): string | null =>
   state.boards.error;
